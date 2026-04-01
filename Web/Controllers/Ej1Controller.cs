8533f34e1bb88dc6f6a347c33eae829e2deb2cc2
julianvecchio1 <julianvecchio1@gmail.com>
2026-04-01 15:15:48 -0300

    Agrega resolucion del ejercicio 1 de POO

--- Web/Controllers/Ej1Controller.cs
+++ Web/Controllers/Ej1Controller.cs
@@ -1,19 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Web.Ej1;
 
 namespace Web.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class Ej1Controller: ControllerBase
+public class Ej1Controller : ControllerBase
 {
-
     [HttpGet]
     public List<string> Get(string persona1, string persona2, string persona3)
     {
-        var l = new List<string>();
-        l.Add("Esto es una prueba");
-        l.Add("Esto es otra prueba");
+        var personas = new List<Persona>
+        {
+            new Persona(persona1),
+            new Persona(persona2),
+            new Persona(persona3)
+        };
 
-        return l;
+        var saludos = new List<string>();
+
+        foreach (var p in personas)
+        {
+            saludos.Add(p.GetSaludo());
+        }
+
+        return saludos;
     }
 }
